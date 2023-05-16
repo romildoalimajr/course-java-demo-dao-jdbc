dde4ba416c7000e9cd959b89e6fb076f00d43e22
Romldo A. Lima Jr <odlimor.sevla@gmail.com>
2023-05-16 09:41:13 -0300

    DepartmentUpdate implemented!

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -33,13 +33,14 @@ public class Program2 {
 		departmentDao.insert(newDepartment);
 		System.out.println("Inserted! New id: " + newDepartment.getId());
 		
-		/*
+	
 		System.out.println("\n===== TEST 4: update =====");
 		Department dep2 = departmentDao.findById(1);
 		dep2.setName("Food");
 		departmentDao.update(dep2);
 		System.out.println("Update completed");
 		
+		/*
 		System.out.println("\n===== TEST 5: delete =====");
 		System.out.print("Enter id for delete test: ");
 		int id = sc.nextInt();
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -6,15 +6,12 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import db.DB;
 import db.DbException;
 import model.dao.DepartmentDao;
 import model.entities.Department;
-import model.entities.Seller;
 
 public class DepartmentDaoJDBC implements DepartmentDao {
 	
@@ -65,7 +62,24 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 	@Override
 	public void update(Department obj) {
 		// TODO Auto-generated method stub
-
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement(
+					"UPDATE department "
+					+ "SET Name = ?"
+					+ "WHERE Id = ?");
+			
+			st.setString(1, obj.getName());
+			st.setInt(2, obj.getId());
+			
+			st.executeUpdate();
+		}
+		catch(SQLException e){
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override
