f00ebb7c3bd711d4fe7a7519851ef76ee8213740
Romldo A. Lima Jr <odlimor.sevla@gmail.com>
2023-05-15 22:19:41 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -8,8 +11,10 @@ public class Program {
 		// TODO Auto-generated method stub
 
 		Department obj = new Department(1, "Books");
-		
+		Seller seller = new Seller(21, "Bob Brown", "bob@gmail.com", new Date(), 3000.00, obj);
 		System.out.println(obj);
+		System.out.println(seller);
+		
 	}
 
 }
